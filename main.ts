76a552bcc811c74922e7b9f0f50fec65565851fc
lmezmor752 <132547384+lmezmor752@users.noreply.github.com>
2024-05-20 10:12:01 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
-basic.forever(function () {
-	
+IR.IR_callbackUser(function (message) {
+    if (message == 22) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+    }
+    if (message == 25) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 100)
+    }
+    if (message == 12) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 150)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+    }
+    if (message == 8) {
+        maqueen.motorStop(maqueen.Motors.All)
+    }
 })
